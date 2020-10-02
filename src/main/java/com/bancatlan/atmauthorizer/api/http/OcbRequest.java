39ee0d6846d6470be37d517acc8f5724fa641153
Jacobo Vasquez <javasquez@bancatlan.hn>
2020-10-01 21:34:19 -0600

    Enabling verification and confirmation endpoint

---
+++ src/main/java/com/bancatlan/atmauthorizer/api/http/OcbRequest.java
@@ -0,0 +1,45 @@
+package com.bancatlan.atmauthorizer.api.http;
+
+import com.bancatlan.atmauthorizer.api.model.Auth;
+import com.bancatlan.atmauthorizer.api.model.SpareParametersDTO;
+import com.bancatlan.atmauthorizer.model.Transaction;
+import com.bancatlan.atmauthorizer.model.Voucher;
+
+public class OcbRequest {
+    private Voucher voucher;
+    private Transaction transaction;
+    private Auth auth;
+    private SpareParametersDTO spareParameters;
+
+    public Voucher getVoucher() {
+        return voucher;
+    }
+
+    public void setVoucher(Voucher voucher) {
+        this.voucher = voucher;
+    }
+
+    public Transaction getTransaction() {
+        return transaction;
+    }
+
+    public void setTransaction(Transaction transaction) {
+        this.transaction = transaction;
+    }
+
+    public Auth getAuth() {
+        return auth;
+    }
+
+    public void setAuth(Auth auth) {
+        this.auth = auth;
+    }
+
+    public SpareParametersDTO getSpareParameters() {
+        return spareParameters;
+    }
+
+    public void setSpareParameters(SpareParametersDTO spareParameters) {
+        this.spareParameters = spareParameters;
+    }
+}
